7d25e30934cf1a07a1935d2f0d79a64758f23ec7
javierCueto <javelie@outlook.com>
2021-06-21 20:51:35 -0600

    set up my table

--- MyChat/Controller/ConversationController.swift
+++ MyChat/Controller/ConversationController.swift
@@ -6,18 +6,35 @@
 //
 
 import UIKit
+private let reuseIdentifier = "ConversationCell"
 
 class ConversationController: UIViewController {
     // MARK: -  Properties
+    private let tableView = UITableView()
+    
     
     
     // MARK: -  Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureTableView()
     }
     
     // MARK: -  Helpers
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        //clear none cells
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        tableView.frame = view.frame
+    }
+    
     
     func configureUI(){
         view.backgroundColor = .white
@@ -52,4 +69,24 @@ class ConversationController: UIViewController {
     @objc func handleShowBotton(){
         print("123tamarindo")
     }
+}
+
+extension ConversationController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension ConversationController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    
 }
