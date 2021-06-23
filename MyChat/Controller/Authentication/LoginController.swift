0ac939bcef9a86923c7321c823c49309d961cd53
javierCueto <javelie@outlook.com>
2021-06-22 21:34:50 -0600

    login part 2, button and containers for inputs

--- MyChat/Controller/Authentication/LoginController.swift
+++ MyChat/Controller/Authentication/LoginController.swift
@@ -16,6 +16,31 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private let emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.setHeight(height: 50)
+        return view
+    }()
+    
+    private let passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.setHeight(height: 50)
+        return view
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setHeight(height: 50)
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        return button
+    }()
+    
     // MARK: -  Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,11 +57,18 @@ class LoginController: UIViewController {
         configureGradientLayer()
         
         configureIcon()
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,passwordContainerView,loginButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
     }
     
     func configureIcon(){
         view.addSubview(iconImage)
-        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 15 ,width: 150, height: 150)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 15 ,width: 120, height: 120)
         iconImage.centerX(inView: view)
     }
     
