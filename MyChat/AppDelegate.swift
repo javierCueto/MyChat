d96ab7f015ecaf76b0c633f5334f27c2620e052e
javierCueto <javelie@outlook.com>
2021-07-01 08:55:19 -0600

    integrate pods file

--- MyChat/AppDelegate.swift
+++ MyChat/AppDelegate.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseMessaging
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+
+         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+         UNUserNotificationCenter.current().requestAuthorization(
+           options: authOptions,
+           completionHandler: {_, _ in })
+        
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
+        
         return true
     }
 
@@ -34,3 +48,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+// MARK: -  <#name#>
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
+    }
+}
+
+
+// MARK: -  name
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        debugPrint("FCM token \(fcmToken)")
+        
+        
+    }
+}
