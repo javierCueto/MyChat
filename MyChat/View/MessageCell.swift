636072a4e84428809ef9cc8284fa700de5eea44d
javierCueto <javelie@outlook.com>
2021-08-08 08:16:15 -0600

    setting up message cells

---
+++ MyChat/View/MessageCell.swift
@@ -0,0 +1,64 @@
+//
+//  MessageCell.swift
+//  MyChat
+//
+//  Created by Javier Cueto on 08/08/21.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    // MARK: -  Properties
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let textView: UITextView = {
+       let tv = UITextView()
+        tv.backgroundColor = .clear
+        tv.font = UIFont.systemFont(ofSize: 16)
+        tv.isScrollEnabled = false
+        tv.isEditable = false
+        tv.textColor = .white
+        tv.text = "my message are here, where your message -.-?"
+        return tv
+    }()
+    
+    private let bubbleContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+       return view
+    }()
+    
+    // MARK: -  Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(left: leftAnchor, bottom: bottomAnchor, paddingLeft: 8, paddingBottom:  -4)
+        profileImageView.setDimensions(height: 32, width: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        addSubview(bubbleContainer)
+        bubbleContainer.layer.cornerRadius = 12
+        bubbleContainer.anchor(top: topAnchor, left: profileImageView.rightAnchor, paddingLeft: 12)
+        bubbleContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        
+        bubbleContainer.addSubview(textView)
+        textView.anchor(top: bubbleContainer.topAnchor, left: bubbleContainer.leftAnchor, bottom: bubbleContainer.bottomAnchor, right: bubbleContainer.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 4, paddingRight: 12)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: -  Helpers
+    
+    
+    // MARK: -  Actions
+}
